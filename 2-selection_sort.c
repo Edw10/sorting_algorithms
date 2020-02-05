8e6fa16329c92e7c0dd660c75d9f8c02fa7af491
Edward Osorno <edward-1094@hotmail.com>
2020-02-05 16:42:07 +0000

    add

---
+++ 2-selection_sort.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * selection_sort - sort an array by selection
+ * @array: the array to sort
+ * @size: size of the @array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, p;
+	int aux;
+
+	for (i = 0; i < (size - 1); i++)
+	{
+		aux = array[i];
+		for (j = (i + 1); j < size; j++)
+		{
+			if (aux > array[j])
+			{
+				aux = array[j];
+				p = j;
+			}
+		}
+		if (aux != array[i])
+		{
+			array[p] = array[i];
+			array[i] = aux;
+			print_array(array, size);
+		}
+	}
+}
